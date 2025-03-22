9379c3fece81ebe9005f50a56900e7ef13c9f407
WangYuli <wangyuli@uniontech.com>
2025-03-22 14:28:36 +0800

    riscv: KGDB: Do not inline arch_kgdb_breakpoint()
    The arch_kgdb_breakpoint() function defines the kgdb_compiled_break
    symbol using inline assembly.
    
    There's a potential issue where the compiler might inline
    arch_kgdb_breakpoint(), which would then define the kgdb_breakinst
    symbol multiple times, leading to fail to link vmlinux.o.
    
    This isn't merely a potential compilation problem. The intent here
    is to determine the global symbol address of kgdb_compiled_break,
    and if this function is inlined multiple times, it would logically
    be a grave error.
    
    Fixes: fe89bd2be866 ("riscv: Add KGDB support")
    Co-developed-by: Huacai Chen <chenhuacai@loongson.cn>
    Signed-off-by: Huacai Chen <chenhuacai@loongson.cn>
    Signed-off-by: WangYuli <wangyuli@uniontech.com>
    Signed-off-by: Linux RISC-V bot <linux.riscv.bot@gmail.com>

--- arch/riscv/include/asm/kgdb.h
+++ arch/riscv/include/asm/kgdb.h
@@ -17,19 +17,8 @@
 #define BREAK_INSTR_SIZE	4
 #endif
 
-#ifndef	__ASSEMBLY__
-
+extern void arch_kgdb_breakpoint(void);
 extern unsigned long kgdb_compiled_break;
-
-static inline void arch_kgdb_breakpoint(void)
-{
-	asm(".global kgdb_compiled_break\n"
-	    ".option norvc\n"
-	    "kgdb_compiled_break: ebreak\n"
-	    ".option rvc\n");
-}
-
-#endif /* !__ASSEMBLY__ */
 
 #define DBG_REG_ZERO "zero"
 #define DBG_REG_RA "ra"
