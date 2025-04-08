f315f168c25e8500ec060f078df44dddaeb85651
Chunyan Zhang <zhangchunyan@iscas.ac.cn>
2025-04-08 16:43:01 +0800

    riscv: mm: Add uffd write-protect support
    The Svrsw60t59b extension allows to free the PTE reserved bits 60 and 59
    for software, this patch uses bit 60 for uffd-wp tracking
    
    Additionally for tracking the uffd-wp state as a PTE swap bit, we borrow
    bit 4 which is not involved into swap entry computation.
    
    Signed-off-by: Chunyan Zhang <zhangchunyan@iscas.ac.cn>
    Signed-off-by: Linux RISC-V bot <linux.riscv.bot@gmail.com>

--- arch/riscv/include/asm/pgtable-bits.h
+++ arch/riscv/include/asm/pgtable-bits.h
@@ -35,6 +35,20 @@
 #define _PAGE_SWP_SOFT_DIRTY	0
 #endif /* CONFIG_MEM_SOFT_DIRTY */
 
+#ifdef CONFIG_HAVE_ARCH_USERFAULTFD_WP
+
+/* ext_svrsw60t59b: Bit(60) for uffd-wp tracking */
+#define _PAGE_UFFD_WP		(1UL << 60)
+/*
+ * Bit 4 is not involved into swap entry computation, so we
+ * can borrow it for swap page uffd-wp tracking.
+ */
+#define _PAGE_SWP_UFFD_WP	_PAGE_USER
+#else
+#define _PAGE_UFFD_WP		0
+#define _PAGE_SWP_UFFD_WP	0
+#endif
+
 #define _PAGE_TABLE     _PAGE_PRESENT
 
 /*
