e38bfe07f2e7c11a8ce2d1e9114f5baa7b3b7cbe
Alexandre Ghiti <alexghiti@rivosinc.com>
2025-04-17 12:54:17 +0000

    riscv: Strengthen duplicate and inconsistent definition of RV_X()
    RV_X() macro is defined in two different ways which is error prone.
    
    So harmonize its first definition and add another macro RV_X_mask() for
    the second one.
    
    Signed-off-by: Alexandre Ghiti <alexghiti@rivosinc.com>

--- arch/riscv/kernel/traps_misaligned.c
+++ arch/riscv/kernel/traps_misaligned.c
@@ -105,7 +105,6 @@
 #define SH_RS2				20
 #define SH_RS2C				2
 
-#define RV_X(x, s, n)			(((x) >> (s)) & ((1 << (n)) - 1))
 #define RVC_LW_IMM(x)			((RV_X(x, 6, 1) << 2) | \
 					 (RV_X(x, 10, 3) << 3) | \
 					 (RV_X(x, 5, 1) << 6))
