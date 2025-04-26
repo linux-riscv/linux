b5c82b1ec8c5195e1d1c1b2be9e38f470074d1a1
Ignacio Encinas <ignacio@iencinas.com>
2025-04-26 16:56:18 +0200

    include/uapi/linux/swab.h: move default implementation for swab macros into asm-generic
    Move the default byteswap implementation into asm-generic so that it can
    be included from arch code.
    
    This is required by RISC-V in order to have a fallback implementation
    without duplicating it.
    
    Signed-off-by: Ignacio Encinas <ignacio@iencinas.com>
    Signed-off-by: Linux RISC-V bot <linux.riscv.bot@gmail.com>

--- include/uapi/asm-generic/swab.h
+++ include/uapi/asm-generic/swab.h
@@ -3,6 +3,7 @@
 #define _ASM_GENERIC_SWAB_H
 
 #include <asm/bitsperlong.h>
+#include <linux/types.h>
 
 /*
  * 32 bit architectures typically (but not always) want to
@@ -15,5 +16,37 @@
 #define __SWAB_64_THRU_32__
 #endif
 #endif
+
+/*
+ * casts are necessary for constants, because we never know how for sure
+ * how U/UL/ULL map to __u16, __u32, __u64. At least not in a portable way.
+ */
+#define ___constant_swab16(x) ((__u16)(				\
+	(((__u16)(x) & (__u16)0x00ffU) << 8) |			\
+	(((__u16)(x) & (__u16)0xff00U) >> 8)))
+
+#define ___constant_swab32(x) ((__u32)(				\
+	(((__u32)(x) & (__u32)0x000000ffUL) << 24) |		\
+	(((__u32)(x) & (__u32)0x0000ff00UL) <<  8) |		\
+	(((__u32)(x) & (__u32)0x00ff0000UL) >>  8) |		\
+	(((__u32)(x) & (__u32)0xff000000UL) >> 24)))
+
+#define ___constant_swab64(x) ((__u64)(				\
+	(((__u64)(x) & (__u64)0x00000000000000ffULL) << 56) |	\
+	(((__u64)(x) & (__u64)0x000000000000ff00ULL) << 40) |	\
+	(((__u64)(x) & (__u64)0x0000000000ff0000ULL) << 24) |	\
+	(((__u64)(x) & (__u64)0x00000000ff000000ULL) <<  8) |	\
+	(((__u64)(x) & (__u64)0x000000ff00000000ULL) >>  8) |	\
+	(((__u64)(x) & (__u64)0x0000ff0000000000ULL) >> 24) |	\
+	(((__u64)(x) & (__u64)0x00ff000000000000ULL) >> 40) |	\
+	(((__u64)(x) & (__u64)0xff00000000000000ULL) >> 56)))
+
+#define ___constant_swahw32(x) ((__u32)(			\
+	(((__u32)(x) & (__u32)0x0000ffffUL) << 16) |		\
+	(((__u32)(x) & (__u32)0xffff0000UL) >> 16)))
+
+#define ___constant_swahb32(x) ((__u32)(			\
+	(((__u32)(x) & (__u32)0x00ff00ffUL) << 8) |		\
+	(((__u32)(x) & (__u32)0xff00ff00UL) >> 8)))
 
 #endif /* _ASM_GENERIC_SWAB_H */
