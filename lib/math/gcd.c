69eafe687878211a59208667961d4a5aab07136c
Kuan-Wei Chiu <visitorckw@gmail.com>
2025-05-24 23:55:17 +0800

    lib/math/gcd: Use static key to select implementation at runtime
    On platforms like RISC-V, the compiler may generate hardware FFS
    instructions even if the underlying CPU does not actually support them.
    Currently, the GCD implementation is chosen at compile time based on
    CONFIG_CPU_NO_EFFICIENT_FFS, which can result in suboptimal behavior on
    such systems.
    
    Introduce a static key, efficient_ffs_key, to enable runtime selection
    between the binary GCD (using ffs) and the odd-even GCD implementation.
    This allows the kernel to default to the faster binary GCD when FFS is
    efficient, while retaining the ability to fall back when needed.
    
    Co-developed-by: Yu-Chun Lin <eleanor15x@gmail.com>
    Signed-off-by: Yu-Chun Lin <eleanor15x@gmail.com>
    Signed-off-by: Kuan-Wei Chiu <visitorckw@gmail.com>
    Signed-off-by: Linux RISC-V bot <linux.riscv.bot@gmail.com>

--- lib/math/gcd.c
+++ lib/math/gcd.c
@@ -2,6 +2,7 @@
 #include <linux/kernel.h>
 #include <linux/gcd.h>
 #include <linux/export.h>
+#include <linux/jump_label.h>
 
 /*
  * This implements the binary GCD algorithm. (Often attributed to Stein,
@@ -11,16 +12,13 @@
  * has decent hardware division.
  */
 
+DEFINE_STATIC_KEY_TRUE(efficient_ffs_key);
+
 #if !defined(CONFIG_CPU_NO_EFFICIENT_FFS)
 
 /* If __ffs is available, the even/odd algorithm benchmarks slower. */
 
-/**
- * gcd - calculate and return the greatest common divisor of 2 unsigned longs
- * @a: first value
- * @b: second value
- */
-unsigned long gcd(unsigned long a, unsigned long b)
+static unsigned long binary_gcd(unsigned long a, unsigned long b)
 {
 	unsigned long r = a | b;
 
@@ -44,15 +42,26 @@ unsigned long gcd(unsigned long a, unsigned long b)
 	}
 }
 
-#else
+#endif
 
 /* If normalization is done by loops, the even/odd algorithm is a win. */
+
+/**
+ * gcd - calculate and return the greatest common divisor of 2 unsigned longs
+ * @a: first value
+ * @b: second value
+ */
 unsigned long gcd(unsigned long a, unsigned long b)
 {
 	unsigned long r = a | b;
 
 	if (!a || !b)
 		return r;
+
+#if !defined(CONFIG_CPU_NO_EFFICIENT_FFS)
+	if (static_branch_likely(&efficient_ffs_key))
+		return binary_gcd(a, b);
+#endif
 
 	/* Isolate lsbit of r */
 	r &= -r;
@@ -79,7 +88,5 @@ unsigned long gcd(unsigned long a, unsigned long b)
 		a >>= 1;
 	}
 }
-
-#endif
 
 EXPORT_SYMBOL_GPL(gcd);
