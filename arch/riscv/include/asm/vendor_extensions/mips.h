dcfb01434af66cdb693cbc9fcd5ff9f92886c2db
Aleksa Paunovic <aleksa.paunovic@htecgroup.com>
2025-06-25 16:20:57 +0200

    riscv: Add xmipsexectl as a vendor extension
    Add support for MIPS vendor extensions. Add support for the xmipsexectl
    vendor extension.
    
    Signed-off-by: Aleksa Paunovic <aleksa.paunovic@htecgroup.com>
    Signed-off-by: Linux RISC-V bot <linux.riscv.bot@gmail.com>

---
+++ arch/riscv/include/asm/vendor_extensions/mips.h
@@ -0,0 +1,16 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * Copyright (C) 2025 MIPS.
+ */
+
+#ifndef _ASM_RISCV_VENDOR_EXTENSIONS_MIPS_H
+#define _ASM_RISCV_VENDOR_EXTENSIONS_MIPS_H
+
+#include <asm/vendor_extensions.h>
+#include <linux/types.h>
+
+#define RISCV_ISA_VENDOR_EXT_XMIPSEXECTL	0
+
+extern struct riscv_isa_vendor_ext_data_list riscv_isa_vendor_ext_list_mips;
+
+#endif // _ASM_RISCV_VENDOR_EXTENSIONS_MIPS_H
