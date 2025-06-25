b0779e10a08d34ad6a26e18c0508d371ba766512
Aleksa Paunovic <aleksa.paunovic@htecgroup.com>
2025-06-25 16:20:58 +0200

    riscv: Add xmipsexectl PAUSE instruction
    Add MIPS.PAUSE instruction opcode. This instruction is a part of the
    xmipsexectl vendor extension.
    
    Signed-off-by: Aleksa Paunovic <aleksa.paunovic@htecgroup.com>
    Signed-off-by: Linux RISC-V bot <linux.riscv.bot@gmail.com>

--- arch/riscv/include/asm/vendor_extensions/mips.h
+++ arch/riscv/include/asm/vendor_extensions/mips.h
@@ -13,4 +13,11 @@
 
 extern struct riscv_isa_vendor_ext_data_list riscv_isa_vendor_ext_list_mips;
 
+/* MIPS.PAUSE is an alternative opcode which is implemented to have the */
+/* same behavior as PAUSE on some MIPS RISCV cores. */
+/* It is a ‘hint’ encoding of the SLLI instruction, */
+/* with rd = 0, rs1 = 0 and imm = 5. */
+
+#define MIPS_PAUSE	".4byte 0x00501013\n\t"
+
 #endif // _ASM_RISCV_VENDOR_EXTENSIONS_MIPS_H
