d547106ed940ac95669622d9a12b1de225a422d4
Fangyu Yu <fangyu.yu@linux.alibaba.com>
2025-08-16 15:32:34 +0800

    RISC-V: KVM: Write hgatp register with valid mode bits
    According to the RISC-V Privileged Architecture Spec, when MODE=Bare
    is selected,software must write zero to the remaining fields of hgatp.
    
    We have detected the valid mode supported by the HW before, So using a
    valid mode to detect how many vmid bits are supported.
    
    Signed-off-by: Fangyu Yu <fangyu.yu@linux.alibaba.com>
    Signed-off-by: Linux RISC-V bot <linux.riscv.bot@gmail.com>

--- arch/riscv/kvm/vmid.c
+++ arch/riscv/kvm/vmid.c
@@ -28,7 +28,7 @@ void __init kvm_riscv_gstage_vmid_detect(void)
 
 	/* Figure-out number of VMID bits in HW */
 	old = csr_read(CSR_HGATP);
-	csr_write(CSR_HGATP, old | HGATP_VMID);
+	csr_write(CSR_HGATP, (kvm_riscv_gstage_mode() << HGATP_MODE_SHIFT) | HGATP_VMID);
 	vmid_bits = csr_read(CSR_HGATP);
 	vmid_bits = (vmid_bits & HGATP_VMID) >> HGATP_VMID_SHIFT;
 	vmid_bits = fls_long(vmid_bits);
