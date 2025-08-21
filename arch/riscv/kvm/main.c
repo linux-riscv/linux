5b0129bde939662361a9d729db4d14de64c375e5
Guo Ren (Alibaba DAMO Academy) <guoren@kernel.org>
2025-08-21 10:25:42 -0400

    RISC-V: KVM: Prevent HGATP_MODE_BARE passed
    urrent kvm_riscv_gstage_mode_detect() assumes H-extension must
    have HGATP_MODE_SV39X4/SV32X4 at least, but the spec allows
    H-extension with HGATP_MODE_BARE alone. The KVM depends on
    !HGATP_MODE_BARE at least, so enhance the gstage-mode-detect
    to block HGATP_MODE_BARE.
    
    Move gstage-mode-check closer to gstage-mode-detect to prevent
    unnecessary init.
    
    Reviewed-by: Troy Mitchell <troy.mitchell@linux.dev>
    Reviewed-by: Nutty Liu <nutty.liu@hotmail.com>
    Signed-off-by: Guo Ren (Alibaba DAMO Academy) <guoren@kernel.org>
    Signed-off-by: Linux RISC-V bot <linux.riscv.bot@gmail.com>

--- arch/riscv/kvm/main.c
+++ arch/riscv/kvm/main.c
@@ -93,6 +93,23 @@ static int __init riscv_kvm_init(void)
 		return rc;
 
 	kvm_riscv_gstage_mode_detect();
+	switch (kvm_riscv_gstage_mode) {
+	case HGATP_MODE_SV32X4:
+		str = "Sv32x4";
+		break;
+	case HGATP_MODE_SV39X4:
+		str = "Sv39x4";
+		break;
+	case HGATP_MODE_SV48X4:
+		str = "Sv48x4";
+		break;
+	case HGATP_MODE_SV57X4:
+		str = "Sv57x4";
+		break;
+	default:
+		return -ENODEV;
+	}
+	kvm_info("using %s G-stage page table format\n", str);
 
 	kvm_riscv_gstage_vmid_detect();
 
@@ -134,24 +151,6 @@ static int __init riscv_kvm_init(void)
 		kvm_info("using SBI nested acceleration with %s\n",
 			 (rc) ? slist : "no features");
 	}
-
-	switch (kvm_riscv_gstage_mode) {
-	case HGATP_MODE_SV32X4:
-		str = "Sv32x4";
-		break;
-	case HGATP_MODE_SV39X4:
-		str = "Sv39x4";
-		break;
-	case HGATP_MODE_SV48X4:
-		str = "Sv48x4";
-		break;
-	case HGATP_MODE_SV57X4:
-		str = "Sv57x4";
-		break;
-	default:
-		return -ENODEV;
-	}
-	kvm_info("using %s G-stage page table format\n", str);
 
 	kvm_info("VMID %ld bits available\n", kvm_riscv_gstage_vmid_bits());
 
