1cd54f87f62231af362c1510c799b2a3edbad622
Jinyu Tang <tjytimi@163.com>
2025-08-25 19:07:08 +0800

    riscv: skip csr restore if vcpu preempted reload
    The kvm_arch_vcpu_load() function is called in two cases for riscv:
    1. When entering KVM_RUN from userspace ioctl.
    2. When a preempted VCPU is scheduled back.
    
    In the second case, if no other KVM VCPU has run on this CPU since the
    current VCPU was preempted, the guest CSR (including AIA CSRS) values
    are still valid in the hardware and do not need to be restored.
    
    This patch is to skip the CSR write path when:
    1. The VCPU was previously preempted
    (vcpu->scheduled_out == 1).
    2. It is being reloaded on the same physical CPU
    (vcpu->arch.last_exit_cpu == cpu).
    3. No other KVM VCPU has used this CPU in the meantime
    (vcpu == __this_cpu_read(kvm_former_vcpu)).
    
    This reduces many CSR writes with frequent preemption on the same CPU.
    
    Signed-off-by: Jinyu Tang <tjytimi@163.com>
    Reviewed-by: Nutty Liu <nutty.liu@hotmail.com>
    Signed-off-by: Linux RISC-V bot <linux.riscv.bot@gmail.com>

--- arch/riscv/kvm/vcpu.c
+++ arch/riscv/kvm/vcpu.c
@@ -25,6 +25,8 @@
 #define CREATE_TRACE_POINTS
 #include "trace.h"
 
+static DEFINE_PER_CPU(struct kvm_vcpu *, kvm_former_vcpu);
+
 const struct _kvm_stats_desc kvm_vcpu_stats_desc[] = {
 	KVM_GENERIC_VCPU_STATS(),
 	STATS_DESC_COUNTER(VCPU, ecall_exit_stat),
@@ -581,6 +583,10 @@ void kvm_arch_vcpu_load(struct kvm_vcpu *vcpu, int cpu)
 	struct kvm_vcpu_csr *csr = &vcpu->arch.guest_csr;
 	struct kvm_vcpu_config *cfg = &vcpu->arch.cfg;
 
+	if  (vcpu == __this_cpu_read(kvm_former_vcpu) &&
+		vcpu->arch.last_exit_cpu == cpu)
+		goto csr_restore_done;
+
 	if (kvm_riscv_nacl_sync_csr_available()) {
 		nsh = nacl_shmem();
 		nacl_csr_write(nsh, CSR_VSSTATUS, csr->vsstatus);
@@ -624,6 +630,9 @@ void kvm_arch_vcpu_load(struct kvm_vcpu *vcpu, int cpu)
 
 	kvm_riscv_mmu_update_hgatp(vcpu);
 
+	kvm_riscv_vcpu_aia_load(vcpu, cpu);
+
+csr_restore_done:
 	kvm_riscv_vcpu_timer_restore(vcpu);
 
 	kvm_riscv_vcpu_host_fp_save(&vcpu->arch.host_context);
@@ -632,8 +641,6 @@ void kvm_arch_vcpu_load(struct kvm_vcpu *vcpu, int cpu)
 	kvm_riscv_vcpu_host_vector_save(&vcpu->arch.host_context);
 	kvm_riscv_vcpu_guest_vector_restore(&vcpu->arch.guest_context,
 					    vcpu->arch.isa);
-
-	kvm_riscv_vcpu_aia_load(vcpu, cpu);
 
 	kvm_make_request(KVM_REQ_STEAL_UPDATE, vcpu);
 
@@ -644,6 +651,8 @@ void kvm_arch_vcpu_put(struct kvm_vcpu *vcpu)
 {
 	void *nsh;
 	struct kvm_vcpu_csr *csr = &vcpu->arch.guest_csr;
+
+	__this_cpu_write(kvm_former_vcpu, vcpu);
 
 	vcpu->cpu = -1;
 
