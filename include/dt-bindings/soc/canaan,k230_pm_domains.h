a55c8314da76e0fe60ca3aedd9474153588ab8ff
jybruceupup <q691800735@163.com>
2025-12-03 17:26:24 +0800

    dt-bindings: soc: Add Kendryte K230 power domain bindings
    Add canaan,k230_pm_domains.h to define power domain IDs for Kendryte K230
    RISC-V SoC (CPU1, AI, display, VPU, 3D-struct engine).
    
    Update MAINTAINERS RISC-V entry to include the new binding header for
    proper maintenance tracking.
    
    Signed-off-by: jybruceupup <q691800735@163.com>
    Signed-off-by: Linux RISC-V bot <linux.riscv.bot@gmail.com>

---
+++ include/dt-bindings/soc/canaan,k230_pm_domains.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#ifndef _DT_BINDINGS_SOC_K230_PM_DOMAINS_H
+#define _DT_BINDINGS_SOC_K230_PM_DOMAINS_H
+
+#define K230_PM_DOMAIN_CPU1       0       /* CPU1 subsystem */
+#define K230_PM_DOMAIN_AI         1       /* AI Subsystem */
+#define K230_PM_DOMAIN_DISP       2       /* Display, 2.5D Engine */
+#define K230_PM_DOMAIN_VPU        3       /* Video subsystem */
+#define K230_PM_DOMAIN_DPU        4       /* 3D-struct Engine */
+#define K230_PM_DOMAIN_MAX        5
+
+#endif /* _DT_BINDINGS_SOC_K230_PM_DOMAINS_H */
