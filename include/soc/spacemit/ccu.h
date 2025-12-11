fa714a722102a32cc112a9d347d130f41b17af10
Yixun Lan <dlan@gentoo.org>
2025-12-11 09:19:44 +0800

    clk: spacemit: k3: add the clock tree
    Add clock support to SpacemiT K3 SoC, the clock tree consist of several
    blocks which are APBC, APBS, APMU, DCIU, MPUM.
    
    Signed-off-by: Yixun Lan <dlan@gentoo.org>
    Signed-off-by: Linux RISC-V bot <linux.riscv.bot@gmail.com>

---
+++ include/soc/spacemit/ccu.h
@@ -0,0 +1,18 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#ifndef __SOC_SPACEMIT_CCU_H__
+#define __SOC_SPACEMIT_CCU_H__
+
+/* Auxiliary device used to represent a CCU reset controller */
+struct spacemit_ccu_adev {
+	struct auxiliary_device adev;
+	struct regmap *regmap;
+};
+
+static inline struct spacemit_ccu_adev *
+to_spacemit_ccu_adev(struct auxiliary_device *adev)
+{
+	return container_of(adev, struct spacemit_ccu_adev, adev);
+}
+
+#endif /* __SOC_SPACEMIT_CCU_H__ */
