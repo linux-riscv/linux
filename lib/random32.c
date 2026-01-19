49fd40d5bb274047ea5593bd157cf8ac09553e72
Ryan Roberts <ryan.roberts@arm.com>
2026-01-19 13:01:09 +0000

    prandom: Add __always_inline version of prandom_u32_state()
    We will shortly use prandom_u32_state() to implement kstack offset
    randomization and some arches need to call it from non-instrumentable
    context. So let's implement prandom_u32_state() as an out-of-line
    wrapper around a new __always_inline prandom_u32_state_inline(). kstack
    offset randomization will use this new version.
    
    Acked-by: Mark Rutland <mark.rutland@arm.com>
    Signed-off-by: Ryan Roberts <ryan.roberts@arm.com>
    Signed-off-by: Linux RISC-V bot <linux.riscv.bot@gmail.com>

--- lib/random32.c
+++ lib/random32.c
@@ -51,13 +51,7 @@
  */
 u32 prandom_u32_state(struct rnd_state *state)
 {
-#define TAUSWORTHE(s, a, b, c, d) ((s & c) << d) ^ (((s << a) ^ s) >> b)
-	state->s1 = TAUSWORTHE(state->s1,  6U, 13U, 4294967294U, 18U);
-	state->s2 = TAUSWORTHE(state->s2,  2U, 27U, 4294967288U,  2U);
-	state->s3 = TAUSWORTHE(state->s3, 13U, 21U, 4294967280U,  7U);
-	state->s4 = TAUSWORTHE(state->s4,  3U, 12U, 4294967168U, 13U);
-
-	return (state->s1 ^ state->s2 ^ state->s3 ^ state->s4);
+	return prandom_u32_state_inline(state);
 }
 EXPORT_SYMBOL(prandom_u32_state);
 
