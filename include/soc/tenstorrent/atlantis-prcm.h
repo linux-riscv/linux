5d2b29bb84a5018b91f619c0972d2a4b0a01588e
Anirudh Srinivasan <asrinivasan@oss.tenstorrent.com>
2026-02-02 13:46:51 -0600

    clk: tenstorrent: Add Atlantis clock controller driver
    Add driver for clock controller in Tenstorrent Atlantis SoC. This version
    of the driver coves clocks from RCPU syscon.
    
    5 types of clocks generated by this controller: PLLs (PLLs
    with bypass functionality and an additional Gate clk at output), Shared
    Gates (Multiple Gate clks that share an enable bit), standard Muxes,
    Dividers and Gates. All clocks are implemented using custom clk ops and
    use the regmap interface associated with the syscon. All clocks are derived
    from a 24 Mhz oscillator.
    
    The reset controller is also setup as an auxiliary device of the clock
    controller.
    
    Signed-off-by: Anirudh Srinivasan <asrinivasan@oss.tenstorrent.com>
    Signed-off-by: Linux RISC-V bot <linux.riscv.bot@gmail.com>

--- include/soc/tenstorrent/atlantis-prcm.h
+++ include/soc/tenstorrent/atlantis-prcm.h
@@ -21,11 +21,38 @@ to_atlantis_prcm_adev(struct auxiliary_device *adev)
 	return container_of(adev, struct atlantis_prcm_adev, adev);
 }
 
+/* RCPU Clock Register Offsets */
+#define RCPU_PLL_CFG_REG	0x0000
+#define RCPU_NOCC_PLL_CFG_REG	0x0004
+#define RCPU_NOCC_CLK_CFG_REG	0x0008
+#define RCPU_DIV_CFG_REG	0x000C
+#define RCPU_BLK_CG_REG		0x0014
+#define LSIO_BLK_CG_REG		0x0018
+#define PLL_RCPU_EN_REG		0x011c
+#define PLL_NOCC_EN_REG		0x0120
+#define BUS_CG_REG		0x01FC
+
 /* RCPU Reset Register Offsets */
 #define RCPU_BLK_RST_REG 0x001c
 #define LSIO_BLK_RST_REG 0x0020
 #define HSIO_BLK_RST_REG 0x000c
 #define PCIE_SUBS_RST_REG 0x0000
 #define MM_RSTN_REG 0x0014
+
+/* PLL Bit Definitions */
+#define PLL_CFG_EN_BIT		BIT(0)
+#define PLL_CFG_BYPASS_BIT	BIT(1)
+#define PLL_CFG_REFDIV_MASK	GENMASK(7, 2)
+#define PLL_CFG_REFDIV_SHIFT	2
+#define PLL_CFG_POSTDIV1_MASK	GENMASK(10, 8)
+#define PLL_CFG_POSTDIV1_SHIFT	8
+#define PLL_CFG_POSTDIV2_MASK	GENMASK(13, 11)
+#define PLL_CFG_POSTDIV2_SHIFT	11
+#define PLL_CFG_FBDIV_MASK	GENMASK(25, 14)
+#define PLL_CFG_FBDIV_SHIFT	14
+#define PLL_CFG_LKDT_BIT	BIT(30)
+#define PLL_CFG_LOCK_BIT	BIT(31)
+#define PLL_LOCK_TIMEOUT_US	1000
+#define PLL_BYPASS_WAIT_US	500
 
 #endif
