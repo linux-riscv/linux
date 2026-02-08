e8ccf98e69dbdd9e578d1ed55e445fb1460ff827
Zong Li <zong.li@sifive.com>
2026-02-07 22:38:36 -0800

    iommu/riscv: create a auxiliary device for HPM
    Create an auxiliary device for HPM when the IOMMU supports a
    hardware performance monitor.
    
    Suggested-by: Samuel Holland <samuel.holland@sifive.com>
    Signed-off-by: Zong Li <zong.li@sifive.com>
    Signed-off-by: Linux RISC-V bot <linux.riscv.bot@gmail.com>

--- drivers/iommu/riscv/iommu.c
+++ drivers/iommu/riscv/iommu.c
@@ -14,6 +14,7 @@
 
 #include <linux/acpi.h>
 #include <linux/acpi_rimt.h>
+#include <linux/auxiliary_bus.h>
 #include <linux/compiler.h>
 #include <linux/crash_dump.h>
 #include <linux/init.h>
@@ -557,6 +558,22 @@ static irqreturn_t riscv_iommu_fltq_process(int irq, void *data)
 	}
 
 	return IRQ_HANDLED;
+}
+
+/*
+ * IOMMU Hardware performance monitor
+ */
+static int riscv_iommu_hpm_enable(struct riscv_iommu_device *iommu)
+{
+	struct auxiliary_device *auxdev;
+
+	/* TODO: for custom event support, the modname should come from compatible */
+	auxdev = __devm_auxiliary_device_create(iommu->dev, KBUILD_MODNAME,
+						"pmu", iommu, 0);
+	if (!auxdev)
+		return -ENODEV;
+
+	return 0;
 }
 
 /* Lookup and initialize device context info structure. */
@@ -1668,6 +1685,9 @@ int riscv_iommu_init(struct riscv_iommu_device *iommu)
 		dev_err_probe(iommu->dev, rc, "cannot register iommu interface\n");
 		goto err_remove_sysfs;
 	}
+
+	if (iommu->caps & RISCV_IOMMU_CAPABILITIES_HPM)
+		riscv_iommu_hpm_enable(iommu);
 
 	return 0;
 
