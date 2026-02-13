85492ef07e5ba56b79e46a2d77f76c103eb6b00f
Wenhong Liu <liu.wenhong35@zte.com.cn>
2026-02-13 17:33:34 +0800

    LRW UART: serial: add driver for the LRW UART
    This commit introduces a serial driver for the LRW UART controller
    
    Key features implemented:
    - Support for FIFO mode (16-byte depth)
    - Baud rate configuration
    - Standard asynchronous communication formats:
      * Data bits: 5, 6, 7, 8, 9 bits
      * Parity: odd, even, fixed, none
      * Stop bits: 1 or 2 bits
    - Hardware flow control (RTS/CTS)
    - Multiple interrupt reporting mechanisms
    
    Signed-off-by: Wenhong Liu <liu.wenhong35@zte.com.cn>
    Signed-off-by: Qingtao Liu <liu.qingtao2@zte.com.cn>
    Signed-off-by: Linux RISC-V bot <linux.riscv.bot@gmail.com>

--- include/uapi/linux/serial_core.h
+++ include/uapi/linux/serial_core.h
@@ -231,6 +231,9 @@
 /* Sunplus UART */
 #define PORT_SUNPLUS	123
 
+/* LRW UART */
+#define PORT_LRW	124
+
 /* Generic type identifier for ports which type is not important to userspace. */
 #define PORT_GENERIC	(-1)
 
