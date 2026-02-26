4a75a35283ef7d992ac4aec4622394152729c2b3
Conor Dooley <conor.dooley@microchip.com>
2026-02-26 11:03:21 +0000

    net: macb: add mpfs specific usrio configuration
    On mpfs the driver needs to make sure the tsu clock source is not the
    fabric, as this requires that the hardware is in Timer Adjust mode,
    which is not compatible with the linux driver trying to control the
    hardware. It is unlikely that this will be set, as the peripheral is
    reset during probe, but if the resets are not provided in devicetree
    it's probable that this bit is set incorrectly, as U-Boot's macb driver
    has the same issue with using usrio settings for at91 platforms as the
    default.
    
    Signed-off-by: Conor Dooley <conor.dooley@microchip.com>
    Signed-off-by: Linux RISC-V bot <linux.riscv.bot@gmail.com>

--- drivers/net/ethernet/cadence/macb.h
+++ drivers/net/ethernet/cadence/macb.h
@@ -780,6 +780,7 @@
 #define MACB_CAPS_RSC				BIT(23)
 #define MACB_CAPS_NO_LSO			BIT(24)
 #define MACB_CAPS_USRIO_HAS_MII			BIT(25)
+#define MACB_CAPS_USRIO_HAS_TSUCLK_SOURCE	BIT(25)
 
 /* LSO settings */
 #define MACB_LSO_UFO_ENABLE			0x01
@@ -1212,6 +1213,7 @@ struct macb_usrio_config {
 	u32 rgmii;
 	u32 refclk;
 	u32 hdfctlen;
+	u32 tsu_source;
 };
 
 struct macb_config {
