b8042a436a8a357862555f4583fee8b6d0b94f10
Charlie Jenkins <thecharlesjenkins@gmail.com>
2026-03-09 18:52:11 -0700

    selftests: riscv: Add braces around EXPECT_EQ()
    EXPECT_EQ() expands to multiple lines, breaking up one-line if
    statements. This issue was not present in the patch on the mailing list
    but was instead introduced by the maintainer when attempting to fix up
    checkpatch warnings. Add braces around EXPECT_EQ() to avoid the error
    even though checkpatch suggests them to be removed:
    
    validate_v_ptrace.c:626:17: error: ‘else’ without a previous ‘if’
    
    Fixes: 3789d5eecd5a ("selftests: riscv: verify syscalls discard vector context")
    Fixes: 30eb191c895b ("selftests: riscv: verify ptrace rejects invalid vector csr inputs")
    Fixes: 849f05ae1ea6 ("selftests: riscv: verify ptrace accepts valid vector csr values")
    Signed-off-by: Charlie Jenkins <thecharlesjenkins@gmail.com>
    Signed-off-by: Linux RISC-V bot <linux.riscv.bot@gmail.com>

--- tools/testing/selftests/riscv/vector/validate_v_ptrace.c
+++ tools/testing/selftests/riscv/vector/validate_v_ptrace.c
@@ -291,10 +291,11 @@ TEST(ptrace_v_syscall_clobbering)
 
 		/* verify initial vsetvli settings */
 
-		if (is_xtheadvector_supported())
+		if (is_xtheadvector_supported()) {
 			EXPECT_EQ(5UL, regset_data->vtype);
-		else
+		} else {
 			EXPECT_EQ(9UL, regset_data->vtype);
+		}
 
 		EXPECT_EQ(regset_data->vlenb, regset_data->vl);
 		EXPECT_EQ(vlenb, regset_data->vlenb);
@@ -620,10 +621,11 @@ TEST_F(v_csr_invalid, ptrace_v_invalid_values)
 
 		/* verify initial vsetvli settings */
 
-		if (is_xtheadvector_supported())
+		if (is_xtheadvector_supported()) {
 			EXPECT_EQ(5UL, regset_data->vtype);
-		else
+		} else {
 			EXPECT_EQ(9UL, regset_data->vtype);
+		}
 
 		EXPECT_EQ(regset_data->vlenb, regset_data->vl);
 		EXPECT_EQ(vlenb, regset_data->vlenb);
@@ -828,10 +830,11 @@ TEST_F(v_csr_valid, ptrace_v_valid_values)
 
 		/* verify initial vsetvli settings */
 
-		if (is_xtheadvector_supported())
+		if (is_xtheadvector_supported()) {
 			EXPECT_EQ(5UL, regset_data->vtype);
-		else
+		} else {
 			EXPECT_EQ(9UL, regset_data->vtype);
+		}
 
 		EXPECT_EQ(regset_data->vlenb, regset_data->vl);
 		EXPECT_EQ(vlenb, regset_data->vlenb);
