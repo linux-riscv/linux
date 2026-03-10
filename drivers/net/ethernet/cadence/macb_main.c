5b87b8e07c8746aa483e1410cbb0a6c340a43b28
Conor Dooley <conor.dooley@microchip.com>
2026-03-10 17:17:13 +0000

    net: macb: add mpfs specific usrio configuration
    On mpfs the driver needs to make sure the tsu clock source is not the
    fabric, as this requires that the hardware is in Timer Adjust mode,
    which is not compatible with the linux driver trying to control the
    hardware. It is unlikely that this will be set, as the peripheral is
    reset during probe, but if the resets are not provided in devicetree
    it's probable that this bit is set incorrectly, as U-Boot's macb driver
    has the same issue with using usrio settings for at91 platforms as the
    default.
    
    Fixes: 8aad66aa59be5 ("net: macb: add polarfire soc reset support")
    Signed-off-by: Conor Dooley <conor.dooley@microchip.com>
    Signed-off-by: Linux RISC-V bot <linux.riscv.bot@gmail.com>

--- drivers/net/ethernet/cadence/macb_main.c
+++ drivers/net/ethernet/cadence/macb_main.c
@@ -4650,6 +4650,9 @@ static int macb_init(struct platform_device *pdev)
 				val |= bp->usrio->refclk;
 		}
 
+		if (bp->caps & MACB_CAPS_USRIO_HAS_TSUCLK_SOURCE)
+			val |= bp->usrio->tsu_source;
+
 		macb_or_gem_writel(bp, USRIO, val);
 	}
 
@@ -5244,6 +5247,10 @@ static const struct macb_usrio_config at91_default_usrio = {
 	.clken = MACB_BIT(CLKEN),
 };
 
+static const struct macb_usrio_config mpfs_usrio = {
+	.tsu_source = 0,
+};
+
 static const struct macb_usrio_config sama7g5_gem_usrio = {
 	.mii = 0,
 	.rmii = 1,
@@ -5376,11 +5383,12 @@ static const struct macb_config zynq_config = {
 static const struct macb_config mpfs_config = {
 	.caps = MACB_CAPS_GIGABIT_MODE_AVAILABLE |
 		MACB_CAPS_JUMBO |
-		MACB_CAPS_GEM_HAS_PTP,
+		MACB_CAPS_GEM_HAS_PTP |
+		MACB_CAPS_USRIO_HAS_TSUCLK_SOURCE,
 	.dma_burst_length = 16,
 	.clk_init = macb_clk_init,
 	.init = init_reset_optional,
-	.usrio = &at91_default_usrio,
+	.usrio = &mpfs_usrio,
 	.max_tx_length = 4040, /* Cadence Erratum 1686 */
 	.jumbo_max_len = 4040,
 };
