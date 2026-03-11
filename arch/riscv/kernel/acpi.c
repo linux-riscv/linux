4be81e232142441b5a8cad507f600383d4e37b5f
Ben Dooks <ben.dooks@codethink.co.uk>
2026-03-11 10:22:17 +0000

    RISC-V: ACPI: fix two __iomem cast warnings
    The __acpi_map_table() and __acpi_unmap_table() return and
    take __iomem tagged pointers. Add a cast to fix the sparse
    warnings:
    
    arch/riscv/kernel/acpi.c:216:30: warning: incorrect type in return expression (different address spaces)
    arch/riscv/kernel/acpi.c:216:30:    expected void [noderef] __iomem *
    arch/riscv/kernel/acpi.c:216:30:    got void *
    arch/riscv/kernel/acpi.c:224:24: warning: incorrect type in argument 1 (different address spaces)
    arch/riscv/kernel/acpi.c:224:24:    expected void *addr
    arch/riscv/kernel/acpi.c:224:24:    got void [noderef] __iomem *map
    
    Signed-off-by: Ben Dooks <ben.dooks@codethink.co.uk>
    Signed-off-by: Linux RISC-V bot <linux.riscv.bot@gmail.com>

--- arch/riscv/kernel/acpi.c
+++ arch/riscv/kernel/acpi.c
@@ -213,7 +213,7 @@ void __init __iomem *__acpi_map_table(unsigned long phys, unsigned long size)
 	if (!size)
 		return NULL;
 
-	return early_memremap(phys, size);
+	return (void __iomem *)early_memremap(phys, size);
 }
 
 void __init __acpi_unmap_table(void __iomem *map, unsigned long size)
@@ -221,7 +221,7 @@ void __init __acpi_unmap_table(void __iomem *map, unsigned long size)
 	if (!map || !size)
 		return;
 
-	early_memunmap(map, size);
+	early_memunmap((void __force *)map, size);
 }
 
 void __iomem *acpi_os_ioremap(acpi_physical_address phys, acpi_size size)
