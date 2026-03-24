b10b55483e1adf9a1c4906afdcac65099ec31651
Christoph Hellwig <hch@lst.de>
2026-03-24 07:21:37 +0100

    xor: assert that xor_blocks is not from preemptible user context
    Most of the optimized xor_blocks versions require FPU/vector registers,
    which generally are not supported in interrupt context.
    
    Both callers already are in user context, so enforce this at the highest
    level.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Linux RISC-V bot <linux.riscv.bot@gmail.com>

--- crypto/xor.c
+++ crypto/xor.c
@@ -28,6 +28,8 @@ xor_blocks(unsigned int src_count, unsigned int bytes, void *dest, void **srcs)
 {
 	unsigned long *p1, *p2, *p3, *p4;
 
+	lockdep_assert_preemption_enabled();
+
 	p1 = (unsigned long *) srcs[0];
 	if (src_count == 1) {
 		active_template->do_2(bytes, dest, p1);
