2ec58365748a0c9cbccb6e0642c08398a8d43481
Thomas Gleixner <tglx@kernel.org>
2026-04-10 14:20:50 +0200

    nios2: Select ARCH_HAS_RANDOM_ENTROPY
    The only remaining non-architecture usage of get_cycles() is to provide
    random_get_entropy().
    
    Switch nios2 over to the new scheme of selecting ARCH_HAS_RANDOM_ENTROPY
    and providing random_get_entropy() in asm/random.h.
    
    Add 'asm/timex.h' includes to the relevant files, so the global include can
    be removed once all architectures are converted over.
    
    Signed-off-by: Thomas Gleixner <tglx@kernel.org>
    Cc: Dinh Nguyen <dinguyen@kernel.org>
    Signed-off-by: Linux RISC-V bot <linux.riscv.bot@gmail.com>

--- arch/nios2/include/asm/timex.h
+++ arch/nios2/include/asm/timex.h
@@ -5,9 +5,6 @@
 #ifndef _ASM_NIOS2_TIMEX_H
 #define _ASM_NIOS2_TIMEX_H
 
-extern cycles_t get_cycles(void);
-#define get_cycles get_cycles
-
-#define random_get_entropy() (((unsigned long)get_cycles()) ?: random_get_entropy_fallback())
+cycles_t get_cycles(void);
 
 #endif
