682d88b8d54137e7591877be5c139dc3eb2462ab
Jinjie Ruan <ruanjinjie@huawei.com>
2026-04-15 17:38:27 +0800

    arch/riscv: Add bitrev.h file to support rev8 and brev8
    The RISC-V Bit-manipulation Extension for Cryptography (Zbkb) provides
    the 'brev8' instruction, which reverses the bits within each byte.
    Combined with the 'rev8' instruction (from Zbb or Zbkb), which reverses
    the byte order of a register, we can efficiently implement 16-bit,
    32-bit, and (on RV64) 64-bit bit reversal.
    
    This is significantly faster than the default software table-lookup
    implementation in lib/bitrev.c, as it replaces memory accesses and
    multiple arithmetic operations with just two or three hardware
    instructions.
    
    Select HAVE_ARCH_BITREVERSE and provide <asm/bitrev.h> to utilize
    these instructions when the Zbkb extension is available at runtime
    via the alternatives mechanism.
    
    Signed-off-by: Jinjie Ruan <ruanjinjie@huawei.com>
    Signed-off-by: Linux RISC-V bot <linux.riscv.bot@gmail.com>

---
+++ arch/riscv/include/asm/bitrev.h
@@ -0,0 +1,41 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef __ASM_BITREV_H
+#define __ASM_BITREV_H
+
+#include <linux/types.h>
+#include <asm/cpufeature-macros.h>
+#include <asm/hwcap.h>
+#include <asm-generic/bitops/__bitrev.h>
+
+static __always_inline __attribute_const__ u32 __arch_bitrev32(u32 x)
+{
+	unsigned long result = x;
+
+	if (!riscv_has_extension_likely(RISCV_ISA_EXT_ZBKB))
+		return generic___bitrev32(x);
+
+	asm volatile(
+		".option push\n"
+		".option arch,+zbkb\n"
+		"rev8 %0, %0\n"
+		"brev8 %0, %0\n"
+		".option pop"
+		: "+r" (result)
+	);
+
+	if (__riscv_xlen == 64)
+		return (u32)(result >> 32);
+
+	return (u32)result;
+}
+
+static __always_inline __attribute_const__ u16 __arch_bitrev16(u16 x)
+{
+	return __arch_bitrev32((u32)x) >> 16;
+}
+
+static __always_inline __attribute_const__ u8 __arch_bitrev8(u8 x)
+{
+	return __arch_bitrev32((u32)x) >> 24;
+}
+#endif
