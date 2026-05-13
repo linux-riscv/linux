386f09c5c8b4dd5abe3385805f82e70b8e063506
Wang Yechao <wang.yechao255@zte.com.cn>
2026-05-13 15:42:51 +0800

    RISC-V: KVM: Split huge pages during KVM_CLEAR_DIRTY_LOG
    Split huge pages on the range specified using KVM_CLEAR_DIRTY_LOG.
    And do not split when enabling dirty logging if
    KVM_DIRTY_LOG_INITIALLY_SET is set.
    
    Signed-off-by: Wang Yechao <wang.yechao255@zte.com.cn>
    Signed-off-by: Linux RISC-V bot <linux.riscv.bot@gmail.com>

--- arch/riscv/kvm/mmu.c
+++ arch/riscv/kvm/mmu.c
@@ -147,6 +147,9 @@ void kvm_arch_mmu_enable_log_dirty_pt_masked(struct kvm *kvm,
 	kvm_riscv_gstage_init(&gstage, kvm);
 
 	kvm_riscv_gstage_wp_range(&gstage, start, end);
+
+	if (kvm_dirty_log_manual_protect_and_init_set(kvm))
+		kvm_riscv_split_huge_pages(&gstage, start, end, true);
 }
 
 void kvm_arch_sync_dirty_log(struct kvm *kvm, struct kvm_memory_slot *memslot)
