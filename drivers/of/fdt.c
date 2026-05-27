883a957a437b99d704054415123cf656f6efb221
Wandun Chen <chenwandun@lixiang.com>
2026-05-27 11:29:13 +0800

    of: reserved_mem: save /memreserve/ entries into the reserved_mem array
    /memreserve/ is used by firmware or bootloaders, such regions hold no
    useful data for crash analysis, they should be excluded from the
    kdump vmcore, so save /memreserve/ entries into the reserved_mem array
    for later exclusion.
    
    If a /memreserve/ entry overlaps any dumpable reserved region, mark
    the whole memreserve entry dumpable as well. This may keep slightly
    more memory in vmcore than strictly necessary, but avoids splitting
    entries and never drops data that may be useful for crash analysis.
    
    Signed-off-by: Wandun Chen <chenwandun@lixiang.com>
    Tested-by: Meijing Zhao <zhaomeijing@lixiang.com>
    Signed-off-by: Linux RISC-V bot <linux.riscv.bot@gmail.com>

--- drivers/of/fdt.c
+++ drivers/of/fdt.c
@@ -499,6 +499,7 @@ void __init early_init_fdt_scan_reserved_mem(void)
 	int n;
 	int res;
 	u64 base, size;
+	int nr_memreserve = 0;
 
 	if (!initial_boot_params)
 		return;
@@ -516,7 +517,9 @@ void __init early_init_fdt_scan_reserved_mem(void)
 		if (!size)
 			break;
 		memblock_reserve(base, size);
+		nr_memreserve++;
 	}
+	fdt_reserved_mem_account_memreserve(nr_memreserve);
 }
 
 /**
@@ -1288,6 +1291,7 @@ void __init unflatten_device_tree(void)
 	if (fdt && alloc_reserved_mem_array()) {
 		/* Save the statically-placed regions in the reserved_mem array */
 		fdt_scan_reserved_mem_late();
+		fdt_reserved_mem_save_memreserve_entries();
 	}
 
 	/* Populate an empty root node when bootloader doesn't provide one */
