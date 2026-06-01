b71a7cb19a71542e419e5b5d9b3ab6e6d40b0f6c
Jinjie Ruan <ruanjinjie@huawei.com>
2026-06-01 17:47:55 +0800

    riscv: kexec_file: Fix TOCTOU buffer overflow via memory region padding
    Sashiko AI code review pointed out there is a TOCTOU (Time-of-Check to
    Time-of-Use) race condition in prepare_elf_headers() between the initial
    pass that counts System RAM ranges and the second pass that populates them.
    If a memory hotplug event occurs between these two steps, the number of
    memory regions may increase, causing an out-of-bounds write to
    the cmem->ranges[] array.
    
    Fix this fundamentally by using `CRASH_HOTPLUG_SAFETY_PADDING` (128 slots)
    to expand the flexible array allocation ceiling upfront. This safely
    absorbs any concurrent memory region expansion. Concurrently, add
    a defensive boundary check inside the callback to return -EAGAIN on
    unexpected overrun, fully eradicating the overflow window and ensuring
    system stability.
    
    Cc: Paul Walmsley <pjw@kernel.org>
    Cc: Palmer Dabbelt <palmer@dabbelt.com>
    Cc: Albert Ou <aou@eecs.berkeley.edu>
    Cc: Alexandre Ghiti <alex@ghiti.fr>
    Cc: songshuaishuai@tinylab.org
    Cc: bjorn@rivosinc.com
    Cc: leitao@debian.org
    Fixes: 8acea455fafa ("RISC-V: Support for kexec_file on panic")
    Reviewed-by: Guo Ren <guoren@kernel.org>
    Signed-off-by: Jinjie Ruan <ruanjinjie@huawei.com>
    Signed-off-by: Linux RISC-V bot <linux.riscv.bot@gmail.com>

--- arch/riscv/kernel/machine_kexec_file.c
+++ arch/riscv/kernel/machine_kexec_file.c
@@ -49,6 +49,9 @@ static int prepare_elf64_ram_headers_callback(struct resource *res, void *arg)
 {
 	struct crash_mem *cmem = arg;
 
+	if (unlikely(cmem->nr_ranges >= cmem->max_nr_ranges))
+		return -EAGAIN;
+
 	cmem->ranges[cmem->nr_ranges].start = res->start;
 	cmem->ranges[cmem->nr_ranges].end = res->end;
 	cmem->nr_ranges++;
@@ -62,7 +65,8 @@ static int prepare_elf_headers(void **addr, unsigned long *sz)
 	unsigned int nr_ranges;
 	int ret;
 
-	nr_ranges = 2; /* For exclusion of crashkernel region */
+	/* For exclusion of crashkernel region */
+	nr_ranges = 2 + CRASH_HOTPLUG_SAFETY_PADDING;
 	walk_system_ram_res(0, -1, &nr_ranges, get_nr_ram_ranges_callback);
 
 	cmem = kmalloc_flex(*cmem, ranges, nr_ranges);
