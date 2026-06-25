6de26d0ee96b75971d2879e50c36cd3a8a714c23
Jesse Taube <jtaubepe@redhat.com>
2026-06-25 09:47:51 -0400

    clocksource/drivers/timer-clint: Add ZICNTR support
    ZICNTR allows the CPU time to be read using CSRs, which used to be the
    default. If ZICNTR is supported prefer using CSRs over MMIO.
    The clint MMIO is covered in the RISC-V ACLINT spec [1], which has
    not been ratified yet, where as the ZICNTR spec is ratified.
    Using the CSRs also used to be the default, but was changed in
    Commit d5be89a8d118 ("RISC-V: Resurrect the MMIO timer implementation
    for M-mode systems") due to K210 not implementing rdtime in M-mode.
    
    [1]: https://github.com/riscvarchive/riscv-aclint
    Signed-off-by: Jesse Taube <jtaubepe@redhat.com>
    Signed-off-by: Linux RISC-V bot <linux.riscv.bot@gmail.com>

--- arch/riscv/include/asm/timex.h
+++ arch/riscv/include/asm/timex.h
@@ -17,17 +17,26 @@ typedef unsigned long cycles_t;
 #ifdef CONFIG_64BIT
 static inline cycles_t get_cycles(void)
 {
+	if (riscv_has_extension_likely(RISCV_ISA_EXT_ZICNTR))
+		return csr_read(CSR_TIME);
+
 	return readq_relaxed(clint_time_val);
 }
 #else /* !CONFIG_64BIT */
 static inline u32 get_cycles(void)
 {
+	if (riscv_has_extension_likely(RISCV_ISA_EXT_ZICNTR))
+		return csr_read(CSR_TIME);
+
 	return readl_relaxed(((u32 *)clint_time_val));
 }
 #define get_cycles get_cycles
 
 static inline u32 get_cycles_hi(void)
 {
+	if (riscv_has_extension_likely(RISCV_ISA_EXT_ZICNTR))
+		return csr_read(CSR_TIMEH);
+
 	return readl_relaxed(((u32 *)clint_time_val) + 1);
 }
 #define get_cycles_hi get_cycles_hi
@@ -40,7 +49,8 @@ static inline u32 get_cycles_hi(void)
  */
 static inline unsigned long random_get_entropy(void)
 {
-	if (unlikely(clint_time_val == NULL))
+	if (!riscv_has_extension_likely(RISCV_ISA_EXT_ZICNTR) &&
+	   (unlikely(clint_time_val == NULL)))
 		return random_get_entropy_fallback();
 	return get_cycles();
 }
