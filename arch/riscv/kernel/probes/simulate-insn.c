154095f21a9fde5dda5804dd7cce9a43c01be3c9
Xiaofeng Yuan <xiaofengmian@163.com>
2026-06-26 06:22:28 +0000

    riscv: probes: simulate c.jal instruction
    The c.jal instruction is currently marked REJECTED in kprobes
    instruction decoding, but it should be SIMULATED like other
    compressed jump instructions.
    
    Add simulate_c_jal() which saves the return address to x1 (ra)
    and sets the program counter to the target offset, reusing
    simulate_c_j for the common jump logic.
    
    Signed-off-by: Xiaofeng Yuan <xiaofengmian@163.com>
    Signed-off-by: Linux RISC-V bot <linux.riscv.bot@gmail.com>

--- arch/riscv/kernel/probes/simulate-insn.c
+++ arch/riscv/kernel/probes/simulate-insn.c
@@ -163,6 +163,14 @@ bool __kprobes simulate_c_j(u32 opcode, unsigned long addr, struct pt_regs *regs
 	return true;
 }
 
+bool __kprobes simulate_c_jal(u32 opcode, unsigned long addr, struct pt_regs *regs)
+{
+	if (!rv_insn_reg_set_val(regs, 1, addr + 2))
+		return false;
+
+	return simulate_c_j(opcode, addr, regs);
+}
+
 static bool __kprobes simulate_c_jr_jalr(u32 opcode, unsigned long addr, struct pt_regs *regs,
 					 bool is_jalr)
 {
