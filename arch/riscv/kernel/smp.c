563e1c84775cbb5b1d536c62fb95c2a2ac35bfa3
Guo Ren (Alibaba DAMO Academy) <guoren@kernel.org>
2026-07-01 03:13:22 +0000

    riscv: smp: Invoke cpu_ops->cpu_stop() in ipi_stop() for hotplug
    When a CPU receives an IPI stop during hotplug teardown, the
    platform-specific cpu_stop() callback should be called before
    spinning in wait_for_interrupt(), so that firmware (e.g. SBI HSM)
    is notified that the hart has stopped.
    
    This aligns ipi_stop() with the existing behavior of
    ipi_cpu_crash_stop(), which already calls cpu_ops[cpu]->cpu_stop()
    under CONFIG_HOTPLUG_CPU.
    
    Signed-off-by: Guo Ren (Alibaba DAMO Academy) <guoren@kernel.org>
    Signed-off-by: Linux RISC-V bot <linux.riscv.bot@gmail.com>

--- arch/riscv/kernel/smp.c
+++ arch/riscv/kernel/smp.c
@@ -82,6 +82,12 @@ int riscv_hartid_to_cpuid(unsigned long hartid)
 static void ipi_stop(void)
 {
 	set_cpu_online(smp_processor_id(), false);
+
+#ifdef CONFIG_HOTPLUG_CPU
+	if (cpu_has_hotplug(cpu))
+		cpu_ops->cpu_stop();
+#endif
+
 	while (1)
 		wait_for_interrupt();
 }
