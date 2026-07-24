895829c5b0839852c1b45437c3ec738b49183f39
Andy Chiu <tchiu@tenstorrent.com>
2026-07-24 11:49:54 -0500

    RISC-V: KVM: fix vcpu vector context handling for kernel-mode vector
    Running vector workloads like perf + mcf on KVM can result in an
    unexpected termination due to a vtype corruption. This happens because
    the kernel-mode vector (KMV) misattributes the guest's vcpu context as
    the user's context and source from a wrong status.VS.
    
    The simplified call chain that results in this problem is shown as
    follow:
    
    __riscv_sys_ioctl()
        kvm_arch_vcpu_ioctl_run()
          kvm_riscv_vcpu_exit()
            kvm_riscv_vcpu_sbi_ecall()
              kvm_riscv_vcpu_pmu_ctr_stop()
                kvm_vcpu_write_guest()
                  __copy_to_user()
                    enter_vector_usercopy()
                      kernel_vector_begin()
    
    kernel_vector_begin() should use the sstatus.VS from guest's vcpu
    context instead of task_pt_reg(current). Also, it should not save
    guest's v-reg into the user's context memory.
    
    To resolve this, the vcpu context must be correctly saved when KMV is
    serving a guest. However, invoking KVM functions directly from generic
    RISC-V architecture code introduces a reverse dependency, breaking
    builds when KVM is configured as N or M.
    
    Address this by registering an RCU-protected callback for context
    flushing. KVM registers this callback at module initialization and
    unregisters it on exit. When KMV starts a kernel context, it can now
    safely flush the vector context via the callback.
    
    Fixes: ecd2ada8a5e0 ("riscv: Add support for kernel mode vector")
    Signed-off-by: Andy Chiu <tchiu@tenstorrent.com>
    Signed-off-by: Linux RISC-V bot <linux.riscv.bot@gmail.com>

--- arch/riscv/kernel/kernel_mode_vector.c
+++ arch/riscv/kernel/kernel_mode_vector.c
@@ -12,16 +12,31 @@
 #include <linux/types.h>
 
 #include <asm/vector.h>
+#include <asm/kvm_vcpu_vector.h>
 #include <asm/switch_to.h>
 #include <asm/simd.h>
 #ifdef CONFIG_RISCV_ISA_V_PREEMPTIVE
 #include <asm/asm-prototypes.h>
 #endif
 
-static inline void riscv_v_flags_set(u32 flags)
+static void (* __rcu kvm_flush_vector_ctx_callback)(void);
+
+void kvm_riscv_register_vctx_callback(void (*func)(void))
 {
-	WRITE_ONCE(current->thread.riscv_v_flags, flags);
+	if (WARN_ON_ONCE(rcu_access_pointer(kvm_flush_vector_ctx_callback)))
+		return;
+
+	rcu_assign_pointer(kvm_flush_vector_ctx_callback, func);
 }
+EXPORT_SYMBOL_GPL(kvm_riscv_register_vctx_callback);
+
+void kvm_riscv_unregister_vctx_callback(void)
+{
+	rcu_assign_pointer(kvm_flush_vector_ctx_callback, NULL);
+	synchronize_rcu();
+}
+EXPORT_SYMBOL_GPL(kvm_riscv_unregister_vctx_callback);
+
 
 static inline void riscv_v_start(u32 flags)
 {
@@ -86,6 +101,22 @@ void put_cpu_vector_context(void)
 }
 EXPORT_SYMBOL_FOR_KVM(put_cpu_vector_context);
 
+static void __riscv_flush_vector_context(void)
+{
+	void (*vcpu_flush_v_callback)(void);
+
+	if (riscv_v_flags() & RISCV_V_VCPU_CTX) {
+		rcu_read_lock();
+		vcpu_flush_v_callback = rcu_dereference(kvm_flush_vector_ctx_callback);
+		vcpu_flush_v_callback();
+		rcu_read_unlock();
+		return;
+	}
+
+	riscv_v_vstate_save(&current->thread.vstate, task_pt_regs(current));
+	riscv_v_vstate_set_restore(current, task_pt_regs(current));
+}
+
 #ifdef CONFIG_RISCV_ISA_V_PREEMPTIVE
 static __always_inline u32 *riscv_v_flags_ptr(void)
 {
@@ -129,7 +160,7 @@ static int riscv_v_stop_kernel_context(void)
 
 static int riscv_v_start_kernel_context(void)
 {
-	struct __riscv_v_ext_state *kvstate, *uvstate;
+	struct __riscv_v_ext_state *kvstate;
 
 	kvstate = &current->thread.kernel_vstate;
 	if (!kvstate->datap)
@@ -147,13 +178,10 @@ static int riscv_v_start_kernel_context(void)
 	}
 
 	/* Transfer the ownership of V from user to kernel, then save */
-	riscv_v_start(RISCV_PREEMPT_V | RISCV_PREEMPT_V_DIRTY);
-	if (__riscv_v_vstate_check(task_pt_regs(current)->status, DIRTY)) {
-		uvstate = &current->thread.vstate;
-		__riscv_v_vstate_save(uvstate, uvstate->datap);
-	}
-	riscv_preempt_v_clear_dirty(current);
-	riscv_v_vstate_set_restore(current, task_pt_regs(current));
+	get_cpu_vector_context();
+	__riscv_flush_vector_context();
+	riscv_v_start(RISCV_PREEMPT_V);
+	put_cpu_vector_context();
 	return 0;
 }
 
@@ -219,8 +247,7 @@ void kernel_vector_begin(void)
 
 	if (riscv_v_start_kernel_context()) {
 		get_cpu_vector_context();
-		riscv_v_vstate_save(&current->thread.vstate, task_pt_regs(current));
-		riscv_v_vstate_set_restore(current, task_pt_regs(current));
+		__riscv_flush_vector_context();
 	}
 
 	riscv_v_enable();
