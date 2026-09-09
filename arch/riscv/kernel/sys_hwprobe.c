b9d7f71af058164501e68215bb20899cf12f3f99
Thomas Weißschuh <thomas.weissschuh@linutronix.de>
2026-09-09 10:51:25 +0200

    riscv/hwprobe: Include vdso/datapage.h
    The symbol vdso_k_arch_data is provided by vdso/datapage.h,
    which is currently not included explicitly. An upcoming refactoring will
    remove the currently used transitive inclusion.
    
    Add an explicit inclusion.
    
    Signed-off-by: Thomas Weißschuh <thomas.weissschuh@linutronix.de>
    Signed-off-by: Linux RISC-V bot <linux.riscv.bot@gmail.com>

--- arch/riscv/kernel/sys_hwprobe.c
+++ arch/riscv/kernel/sys_hwprobe.c
@@ -18,6 +18,7 @@
 #include <asm/vendor_extensions/mips_hwprobe.h>
 #include <asm/vendor_extensions/sifive_hwprobe.h>
 #include <asm/vendor_extensions/thead_hwprobe.h>
+#include <vdso/datapage.h>
 #include <vdso/vsyscall.h>
 
 
