e0a2010e40a35781dce65a77aea7e58165970715
GUO Ren (XuanTie) <guoren@kernel.org>
2026-09-21 12:27:04 +0000

    RISC-V: paravirt: Support nopvspin to disable PARAVIRT_SPINLOCKS
    VM guests should fall back to a Test-and-Set spinlock when
    PARAVIRT_SPINLOCKS is disabled, because fair locks suffer from severe
    lock-holder preemption issues. The virt_spin_lock_key shortcuts
    queued_spin_lock_slowpath(), allowing virt_spin_lock() to hijack it.
    See commit 43b3f02899f7 ("locking/qspinlock/x86: Fix performance
    regression under unaccelerated VMs").
    
    Add a static key controlling whether virt_spin_lock() is called, and
    add nopvspin support mirroring x86.
    
    Signed-off-by: GUO Ren (XuanTie) <guoren@kernel.org>
    Signed-off-by: Linux RISC-V bot <linux.riscv.bot@gmail.com>

--- arch/riscv/include/asm/qspinlock.h
+++ arch/riscv/include/asm/qspinlock.h
@@ -12,6 +12,8 @@
 /* How long a lock should spin before we consider blocking */
 #define SPIN_THRESHOLD		(1 << 15)
 
+extern bool nopvspin;
+
 void native_queued_spin_lock_slowpath(struct qspinlock *lock, u32 val);
 void __pv_init_lock_hash(void);
 void __pv_queued_spin_lock_slowpath(struct qspinlock *lock, u32 val);
@@ -29,5 +31,27 @@ static inline void queued_spin_unlock(struct qspinlock *lock)
 #endif /* CONFIG_PARAVIRT_SPINLOCKS */
 
 #include <asm-generic/qspinlock.h>
+#include <asm/jump_label.h>
+
+/*
+ * KVM guests fall back to a Test-and-Set spinlock because fair locks suffer
+ * from severe lock-holder-preemption issues. When virt_spin_lock_key is
+ * enabled, virt_spin_lock() shortcuts queued_spin_lock_slowpath() and hijacks
+ * the lock acquisition.
+ */
+DECLARE_STATIC_KEY_FALSE(virt_spin_lock_key);
+
+#define virt_spin_lock rv_virt_spin_lock
+static inline bool rv_virt_spin_lock(struct qspinlock *lock)
+{
+	if (!static_branch_likely(&virt_spin_lock_key))
+		return false;
+
+	do {
+		smp_cond_load_relaxed((s32 *)&lock->val, VAL == 0);
+	} while (atomic_cmpxchg(&lock->val, 0, _Q_LOCKED_VAL) != 0);
+
+	return true;
+}
 
 #endif /* _ASM_RISCV_QSPINLOCK_H */
